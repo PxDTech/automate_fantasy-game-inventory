cabbbea3933882fd8079abe42a3a6e9867e19b06
PxDTech <118567236+PxDTech@users.noreply.github.com>
2023-11-30 06:36:24 -0500

    Add files via upload

--- fantasy-game-inventory.py
+++ fantasy-game-inventory.py
@@ -6,6 +6,19 @@ def displayInventory(inventory):
         item_total += v
     print("Total number of items: " + str(item_total))
 
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        #print(item)
+        if item not in inventory.keys():
+            inventory.update({item: 0})
+        if item in inventory.keys():
+            new_value = inventory.get(item)
+            new_value += 1
+            inventory[item] = new_value
+    return inventory
 
-displayInventory(stuff)
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+#print(inv)
+displayInventory(inv)
